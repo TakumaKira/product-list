8afee2998f257099e90f6fcfa2be5570c83fd191
Takuma Kira <takuma@kirakiraworx.com>
2021-10-03 15:45:28 +0900

    refactor: Simplify function components

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,13 +1,10 @@
-const SearchBox = ({ value, onChange }: { value: string, onChange: Function }) => {
-  return (
-    <input
-      type="text"
-      className="form-control my-3"
-      placeholder="Search by products name..."
-      value={value}
-      onChange={e => onChange(e.currentTarget.value)}
-    />
-  );
-};
+const SearchBox = ({ value, onChange }: { value: string, onChange: Function }) =>
+  <input
+    type="text"
+    className="form-control my-3"
+    placeholder="Search by products name..."
+    value={value}
+    onChange={e => onChange(e.currentTarget.value)}
+  />;
 
 export default SearchBox;
